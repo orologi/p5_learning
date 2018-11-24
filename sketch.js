f46fa9956898c18f0a93c3581677114ed00de6dc
oro.logi <oro.logi@outlook.com>
2018-11-24 14:22:23 +0100

    Signed-off-by: oro.logi <oro.logi@outlook.com>

--- sketch.js
+++ sketch.js
@@ -2,39 +2,37 @@ var canvas;
 var h1;
 var i = 0;
 var bgcolour;
+var fontsize = 50;
 
 
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(255, 255);
 	h1 = createElement("h1", "Waiting.");
-    bgcolour = color(200)
+    bgcolour = color(200,200,100)
     var button = createButton("Click on me for my fav num");
     button.mousePressed(ButtonAction)
 }
 
 function ButtonAction(){
-	
-    
     h1.html("now I will show you my fav number");
     if (i <= 5){
-       
         i = i + 1;
-        bgcolour = color(random(255));
+        bgcolour = color(random(200),random(200),random(200));
     }else  {
-        i = ("no more fav num");
+        i = (0);
     }
 }
 
 
 function drawNum(){
     fill(0);
+    textSize(fontsize);
 	text(i, 100, 100);
 }
 
 
 function draw(x) {
   background(bgcolour);
-  textAlign(RIGHT);
-
-  drawNum( width  );
+  textAlign(CENTER);
+  drawNum( fontsize );
 }
