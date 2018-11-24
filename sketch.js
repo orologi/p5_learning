3519cba9b5748bcd51dbd860b64c0afb01aeab84
oro.logi <oro.logi@outlook.com>
2018-11-24 13:04:51 +0100

    changed where number appears
    Signed-off-by: oro.logi <oro.logi@outlook.com>

--- sketch.js
+++ sketch.js
@@ -1,34 +1,40 @@
 var canvas;
 var h1;
-
 var i = 0;
+var bgcolour;
+
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(200, 200);
 	h1 = createElement("h1", "Waiting.");
+    bgcolour = color(200)
+    var button = createButton("Click on me for my fav num");
+    button.mousePressed(ButtonAction)
 }
 
-
-function mousePressed(){
+function ButtonAction(){
+	
     
-	h1.html("now I will show you my fav number");
+    h1.html("now I will show you my fav number");
     if (i <= 5){
-        createP ("My fav num " +i);
-        i = i+1;
-    }else if (i <= 6) {
-        createP("no more fav num").addClass('text');
-    	i = i+1;
-        console.log(i)
-        
-    }else if(i = 7){
-    //pass
+       
+        i = i + 1;
+        bgcolour = color(random(255));
+    }else  {
+        i = ("no more fav num");
     }
-    
 }
 
 
+function drawNum(){
+    fill(0);
+	text(i, 100, 100);
+}
 
-function draw() {
-  background(220);
-  
+
+function draw(x) {
+  background(bgcolour);
+  textAlign(RIGHT);
+
+  drawNum( width  );
 }
