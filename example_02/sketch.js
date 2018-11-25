94cb7c448d4d4943a0cd80612485caa11e4f2f69
orologi <32539403+orologi@users.noreply.github.com>
2018-11-25 17:42:48 +0100

    corrections

--- example_02/sketch.js
+++ example_02/sketch.js
@@ -1,13 +1,12 @@
-var paddingBox = 150
-var xPosition = 500 - paddingBox;
-var yPosition = 200 - paddingBox;
+var xPosition = 350;
+var yPosition = 10;
 var sBHeight = 100;
 var sBLength = 100;
 var BBHeight = 100;
 var BBLength = 300;
-var canvasY= 200;
+var canvasY = 120;
 var canvasX = 500;
-
+var letterDposY = 70;
 
 
 function setup() {
@@ -18,25 +17,25 @@ function setup() {
 function draw() {
     background(0)
     if (mouseX > xPosition && mouseX < xPosition + sBLength && mouseY < yPosition + sBHeight && mouseY > yPosition) {
-         bigSquare = rect(150, yPosition, BBLength, BBHeight, 30)
+        bigSquare = rect(150, yPosition, BBLength, BBHeight, 30)
         fill(0, 0, 0);
         textSize(18);
-        text("Share", 365, 110);
+        text("Share", 365, letterDposY);
         fill(255, 0, 0);
-        text('ꓷ', 413, 110);
+        text('ꓷ', 413, letterDposY);
         fill(255, 255, 255);
-        
-       
-        
+
+
+
     } else {
         noFill();
         textSize(18);
-      
-          fill(255);
+
+        fill(255);
         //noFill(100);
         smallSquare = rect(xPosition, yPosition, sBLength, sBHeight, 30)
-  		fill(0)
-        text("Share", 365, 110);
-        text('D', 413, 110);
+        fill(0)
+        text("Share", 365, letterDposY);
+        text('D', 413, letterDposY);
     }
 }
