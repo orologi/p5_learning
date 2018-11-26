7dba2e2cf6f0e78ce003d2cb209bb989503252c8
orologi <32539403+orologi@users.noreply.github.com>
2018-11-26 18:54:01 +0100

    cleaned up code

--- example_02/sketch.js
+++ example_02/sketch.js
@@ -1,43 +1,45 @@
-var xPosition = 350;
-var yPosition = 10;
-var sBHeight = 100;
-var sBLength = 100;
-var BBHeight = 100;
-var BBLength = 300;
 var canvasY = 120;
 var canvasX = 500;
-var letterDposY = 70;
 
+var textPosition = {
+    x: 365,
+    y: 70,
+}
+
+var boxPosition = {
+    x: 350,
+    y: 10,
+    dx: 413,
+}
+
+var boxSize = {
+    height: 100,
+    sLength: 100,
+    bLength: 300,
+
+}
 
 function setup() {
     createCanvas(canvasX, canvasY);
 }
 
-
 function draw() {
     background(0)
-    if (mouseX > xPosition && mouseX < xPosition + sBLength && mouseY < yPosition + sBHeight && mouseY > yPosition) {
-        bigSquare = rect(150, yPosition, BBLength, BBHeight, 30)
+    if (mouseX > boxPosition.x && mouseX < boxPosition.x + boxSize.sLength && mouseY < boxPosition.y + boxSize.height && mouseY > boxPosition.y) {
+        bigSquare = rect(150, boxPosition.y, boxSize.bLength, boxSize.height, 30)
         fill(0, 0, 0);
         textSize(18);
-        text("Share", 365, letterDposY);
+        text("Share", textPosition.x, textPosition.y);
         fill(255, 0, 0);
-        text('ꓷ', 413, letterDposY);
+        text('ꓷ', 413, textPosition.y);
         fill(255, 255, 255);
-
-
-
     } else {
         noFill();
         textSize(18);
-
         fill(255);
-        //noFill(100);
-        smallSquare = rect(xPosition, yPosition, sBLength, sBHeight, 30)
+        smallSquare = rect(boxPosition.x, boxPosition.y, boxSize.sLength, boxSize.height, 30)
         fill(0)
-        text("Share", 365, letterDposY);
-        text('D', 413, letterDposY);
+        text("Share", textPosition.x, textPosition.y);
+        text('D', 413, textPosition.y);
     }
 }
-
-console.log('%c Myfriends', 'color:orange; font-weight: bold;')
