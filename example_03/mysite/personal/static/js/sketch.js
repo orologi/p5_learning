f32aec3ad3d5c8929db2f51bb4cae7c5209ddc80
orologi <32539403+orologi@users.noreply.github.com>
2018-11-28 19:10:45 +0100

    added menu for example_03

---
+++ example_03/mysite/personal/static/js/sketch.js
@@ -0,0 +1,49 @@
+var canvasY = 120;
+var canvasX = 500;
+
+var textPosition = {
+    x: 365,
+    y: 70,
+}
+
+var boxPosition = {
+    x: 350,
+    y: 10,
+    dx: 413,
+}
+
+var boxSize = {
+    height: 100,
+    sLength: 100,
+    bLength: 300,
+
+}
+
+function setup() {
+    createCanvas(canvasX, canvasY);
+}
+
+function draw() {
+    background(0);
+    buttonInteraction();
+}
+
+function buttonInteraction() {
+    if (mouseX > boxPosition.x && mouseX < boxPosition.x + boxSize.sLength && mouseY < boxPosition.y + boxSize.height && mouseY > boxPosition.y) {
+        bigSquare = rect(150, boxPosition.y, boxSize.bLength, boxSize.height, 30)
+        fill(0, 0, 0);
+        textSize(18);
+        text("Share", textPosition.x, textPosition.y);
+        fill(255, 0, 0);
+        text('ꓷ', 413, textPosition.y);
+        fill(255, 255, 255);
+    } else {
+        noFill();
+        textSize(18);
+        fill(255);
+        smallSquare = rect(boxPosition.x, boxPosition.y, boxSize.sLength, boxSize.height, 30)
+        fill(0)
+        text("Share", textPosition.x, textPosition.y);
+        text('D', 413, textPosition.y);
+    }
+}
